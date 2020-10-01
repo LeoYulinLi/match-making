84b96ecbd061db7830067e5f553a8c79225458e3
LeoYulinLi <LeoYulinLi@gmail.com>
2020-10-01 14:59:05 -0700

    Fixed a complain from tsc

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import * as io from "socket.io";
 import mongoose from "mongoose";
 import keys from "./keys/keys";
 import User from "./models/User";
+import { emit } from 'cluster';
 
-const server = io.listen(process.env.PORT || 3000)
+const server = io.listen(process.env.PORT || 3001)
 
 if (keys.mongoURI) {
   mongoose
@@ -40,42 +41,82 @@ interface Player {
 
 interface Config {
   threshold: number
+  bucketSize: number
 }
 
+interface RequestOption {
+  teamSize: number
+}
+
+// Team1: [1, 4], 2
+// Team2: 1, 3, 2
 
 server.on("connection", (socket) => {
 
   let players: Player[] = []
 
+  const buckets: Record<number, string[]> = {}
+
   let config: Config = {
-    threshold: 0.2
+    threshold: 0.2,
+    bucketSize: 0.1
+
+  }
+
+  function getBucket(player: Player) {
+    console.log(config.bucketSize)
+    const result = Math.floor(player.level / config.bucketSize)
+    console.log(`${player.id} is at bucket ${result}`)
+    return result
   }
 
   function matchPlayer(newPlayer: Player) {
     console.log(`enqueued player with id ${ newPlayer.id } at level ${ newPlayer.level }`)
-    const matchingPlayerIndex = players.findIndex((player) => {
-      return player.level - config.threshold < newPlayer.level && player.level + config.threshold > newPlayer.level
-    })
-    if (matchingPlayerIndex != -1) {
-      socket.emit("match", [newPlayer, players[matchingPlayerIndex]])
-      console.log(`matched player ${ newPlayer.id } and ${ players[matchingPlayerIndex].id }`)
-      delete players[matchingPlayerIndex]
+    players.push(newPlayer)
+    const bucket = getBucket(newPlayer)
+    if (buckets[bucket]) {
+      buckets[bucket].push(newPlayer.id)
     } else {
-      players.push(newPlayer)
+      buckets[bucket] = [newPlayer.id]
     }
   }
 
   socket.on("enqueue", matchPlayer)
+
+  // TODO: bucket
   socket.on("dequeue", (player: Player) => {
     delete players[players.findIndex(it => it.id === player.id)]
   })
 
   socket.on("config", (newConfig: Config) => {
-    config = newConfig
+    config = { ...config, ...newConfig }
     const playersCopy = [...players]
     players = []
     playersCopy.forEach(player => matchPlayer(player))
   })
 
 
+  socket.on("requestMatch", (option: RequestOption) =>{
+    console.log("requestMatch")
+    console.log(option)
+
+    const firstPlayer = players[0]
+
+    const bucket = getBucket(firstPlayer)
+
+    if (buckets[bucket].length >= option.teamSize * 2) {
+
+      const result = []
+
+      for (let i = 0; i < option.teamSize * 2; i++) {
+        result.push(buckets[bucket].pop())
+      }
+      console.log("match")
+      console.log(result)
+    } else {
+      console.log("standby")
+      emit("standby", "")
+    }
+
+  })
 })
--- test-client/src/app.ts
+++ test-client/src/app.ts
@@ -1,6 +1,6 @@
 import client from "socket.io-client"
 
-const socket = client.connect("http://localhost:3000?apiKey=asdf")
+const socket = client.connect("http://localhost:3001?apiKey=asdf")
 
 socket.on("connect", () => {
   socket.on("match", (data: any) => {
@@ -18,10 +18,11 @@ socket.on("connect", () => {
   }, 1000)
 })
 
-const socket2 = client.connect("http://localhost:3000?apiKey=defg")
+const socket2 = client.connect("http://localhost:3001?apiKey=defg")
 
 socket2.on("connect", () => {
   socket2.on("match", (data: any) => {
+    console.log("matched")
     console.log(data)
   })
   socket2.emit("enqueue", {
@@ -30,7 +31,26 @@ socket2.on("connect", () => {
   })
   socket2.emit("enqueue", {
     id: "defg",
-    level: 0.5
+    level: 0.6
   })
+  socket2.emit("enqueue", {
+    id: "ns",
+    level: 0.7
+  })
+  socket2.emit("enqueue", {
+    id: "cnbv",
+    level: 0.8
+  })
+  socket2.emit("enqueue", {
+    id: "ryeh",
+    level: 0.9
+  })
+  socket2.emit("enqueue", {
+    id: "ewryg",
+    level: 0.24
+  })
+  socket2.emit("requestMatch", { teamSize: 1 })
+  console.log("requesting match")
   socket2.emit("config", { threshold: 0.3 })
+  socket2.on("standby", () => console.log("standby"))
 })
